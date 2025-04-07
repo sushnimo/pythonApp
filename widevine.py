8fe63706ae015c318a905b6a4383305c4363a4e3
sushant <sushantnimoria077@gmail.com>
2025-04-08 00:34:00 +0530

    commit-widevine

---
+++ widevine.py
@@ -0,0 +1,34 @@
+from pywidevine.cdm import Cdm
+from pywidevine.device import Device
+from pywidevine.pssh import PSSH
+
+import requests
+
+# prepare pssh
+pssh = PSSH("AAAAMnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAABISEEmnzZpf3VPTm2bOJoR652I=")
+
+# load device
+device = Device.load("C:/Users/Sushant/Desktop/pywidevine/device.wvd")
+
+# load cdm
+cdm = Cdm.from_device(device)
+
+# open cdm session
+session_id = cdm.open()
+
+# get license challenge
+challenge = cdm.get_license_challenge(session_id, pssh)
+
+# send license challenge (assuming a generic license server SDK with no API front)
+licence = requests.post("https://tataplay.live.ott.irdeto.com/Widevine/getlicense?CrmId=tatasky&AccountId=tatasky&ContentId=400000007&ls_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNvbnRyb2xfc2lnbmluZ19rZXlfcHJvZHVjdGlvbl8xNzIyOTY3MDk3ODc1In0.eyJzdWIiOiIxMTcyNTEyNzcyIiwiaXNlIjp0cnVlLCJqdGkiOiJhYzhiNWEzMS1lMGNhLTRhNTctYjRkNS0wNGRiNjdlOWIzOGEiLCJhaWQiOiJ0YXRhc2t5IiwiZXhwIjoxNzQ0MDU3MDI3LCJuYW1lIjoidXNoYSAuIiwiaWF0IjoxNzQ0MDQyMzI3LCJlbnQiOlt7ImVwaWQiOiJTdWJzY3JpcHRpb25fQnJvd3Nlcl9TdHJlYW1pbmciLCJiaWQiOiIxMDAwMDAxMzQyIn1dLCJpc3MiOiJ0cG1hX3dlYiJ9.10jJGSOgS0fKatHed67TbQxlST7-wyKAwzJfmZS51ks", data=challenge)
+licence.raise_for_status()
+
+# parse license challenge
+cdm.parse_license(session_id, licence.content)
+
+# print keys
+for key in cdm.get_keys(session_id):
+    print(f"[{key.type}] {key.kid.hex}:{key.key.hex()}")
+
+# close session, disposes of session data
+cdm.close(session_id)
