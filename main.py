b7fd81d332ba5099c01cc1a6348819a4b157ab0c
sushnimo <33617992+sushnimo@users.noreply.github.com>
2025-04-09 01:40:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ session_id = cdm.open()
 challenge = cdm.get_license_challenge(session_id, pssh)
 
 # send license challenge (assuming a generic license server SDK with no API front)
-licence = requests.post("http://jtv.jonatv.website/tsky/widevine.php?id=78", data=challenge)
+licence = requests.post("https://tataplay.live.ott.irdeto.com/Widevine/getlicense?CrmId=tatasky&AccountId=tatasky&ContentId=400000077&ls_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNvbnRyb2xfc2lnbmluZ19rZXlfcHJvZHVjdGlvbl8xNzIyOTY3MDk3ODc1In0.eyJzdWIiOiIxMTcyNTEyNzcyIiwiaXNlIjp0cnVlLCJqdGkiOiJhYjMyNTFjNi1mMjMwLTQ3ZGEtOWNkNC04MzJkNjJkMjJkNWQiLCJhaWQiOiJ0YXRhc2t5IiwiZXhwIjoxNzQ0MTU2MDI4LCJuYW1lIjoidXNoYSAuIiwiaWF0IjoxNzQ0MTQxMzI4LCJlbnQiOlt7ImVwaWQiOiJTdWJzY3JpcHRpb25fQnJvd3Nlcl9TdHJlYW1pbmciLCJiaWQiOiIxMDAwMDAwOTI3In1dLCJpc3MiOiJ0cG1hX3dlYiJ9.JhVim8CpYop_YDczlw3o8CW1jpmcbj2Mg_4PXklotyQ", data=challenge)
 licence.raise_for_status()
 
 # parse license challenge
