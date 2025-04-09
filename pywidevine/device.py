9195d6dfd6883e5fcf1fda66f9bc7feb99c0f43f
sushnimo <33617992+sushnimo@users.noreply.github.com>
2025-04-09 13:52:54 +0530

    Update device.py

--- pywidevine/device.py
+++ pywidevine/device.py
@@ -33,7 +33,7 @@ class _Structures:
     # - Removed vmp and vmp_len as it should already be within the Client ID
     v2 = Struct(
         "signature" / magic,
-        "version" / Const(1, Int8ub),
+        "version" / Const(Int8ub, 1),
         "type_" / CEnum(
             Int8ub,
             **{t.name: t.value for t in DeviceTypes}
@@ -52,7 +52,7 @@ class _Structures:
     # ✅ Fixed version field here:
     v1 = Struct(
         "signature" / magic,
-        "version" / Const(1, Int8ub),  # <-- Corrected here
+        "version" / Const(Int8ub, 1),  # <-- Corrected here
         "type_" / CEnum(
             Int8ub,
             **{t.name: t.value for t in DeviceTypes}
