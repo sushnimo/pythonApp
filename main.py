ee6d1279c32e86ab61976dcc57132c69ef3e2c2d
sushnimo <33617992+sushnimo@users.noreply.github.com>
2025-04-10 01:29:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ if __name__ == '__main__':
     # print(f" * Access the API at: {public_url}/get-keys")
 
     # Run the Flask app
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
