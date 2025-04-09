43c779eca779cd68bea4ec57a6142f0d10a1c378
sushnimo <33617992+sushnimo@users.noreply.github.com>
2025-04-09 14:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pywidevine.pssh import PSSH
 import requests
 
 # prepare pssh
-pssh = PSSH("AAAAMnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAABISEKPlJ4mcfldmnVmKlzbDDMg=")
+pssh = PSSH("AAAAMnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAABISEH+rmg6bZFaJn4MHwFn9Ft4=")
 
 # load device
 device = Device.load("device.wvd")
@@ -20,7 +20,7 @@ session_id = cdm.open()
 challenge = cdm.get_license_challenge(session_id, pssh)
 
 # send license challenge (assuming a generic license server SDK with no API front)
-licence = requests.post("https://tataplay.live.ott.irdeto.com/Widevine/getlicense?CrmId=tatasky&AccountId=tatasky&ContentId=400000077&ls_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNvbnRyb2xfc2lnbmluZ19rZXlfcHJvZHVjdGlvbl8xNzIyOTY3MDk3ODc1In0.eyJzdWIiOiIxMTcyNTEyNzcyIiwiaXNlIjp0cnVlLCJqdGkiOiI2MzNkMmU2ZC0yOGI0LTQwYTctOGU0Ny0zNGVjMmJkNjczYzEiLCJhaWQiOiJ0YXRhc2t5IiwiZXhwIjoxNzQ0MTEyMjg4LCJuYW1lIjoidXNoYSAuIiwiaWF0IjoxNzQ0MDk3NTg4LCJlbnQiOlt7ImVwaWQiOiJTdWJzY3JpcHRpb25fQnJvd3Nlcl9TdHJlYW1pbmciLCJiaWQiOiIxMDAwMDAwOTI3In1dLCJpc3MiOiJ0cG1hX3dlYiJ9.RRPpn-pVTO9rjJpWzcfca_QUXM-vIWS9rrlBqkYjXPE", data=challenge)
+licence = requests.post("https://tataplay.live.ott.irdeto.com/Widevine/getlicense?CrmId=tatasky&AccountId=tatasky&ContentId=400000077&ls_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNvbnRyb2xfc2lnbmluZ19rZXlfcHJvZHVjdGlvbl8xNzIyOTY3MDk3ODc1In0.eyJzdWIiOiIxMTcyNTEyNzcyIiwiaXNlIjp0cnVlLCJqdGkiOiJhYTBkY2JlNC1mZDljLTRhZmMtOTMyNi0yYTIyYWI0MTk5OWEiLCJhaWQiOiJ0YXRhc2t5IiwiZXhwIjoxNzQ0MjAyOTAzLCJuYW1lIjoidXNoYSAuIiwiaWF0IjoxNzQ0MTg4MjAzLCJlbnQiOlt7ImVwaWQiOiJTdWJzY3JpcHRpb25fQnJvd3Nlcl9TdHJlYW1pbmciLCJiaWQiOiIxMDAwMDAwOTI3In1dLCJpc3MiOiJ0cG1hX3dlYiJ9.b_XBmEcOxbEJuSkSTGVupFTcPv-RhrR5R6j6UzvrXU4", data=challenge)
 licence.raise_for_status()
 
 # parse license challenge
