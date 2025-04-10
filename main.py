8997989269e11817fe033548ae1e65c695f12bba
sushnimo <33617992+sushnimo@users.noreply.github.com>
2025-04-10 23:34:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,17 @@ def get_keys() -> Response:
         # get license challenge
         challenge = cdm.get_license_challenge(session_id, pssh)
 
+        proxies = {
+            "http": "http://103.216.82.198:6667",
+            "https": "http://103.216.82.198:6667"
+        }
+
         # send license challenge
         licence = requests.post(
             "https://tataplay.live.ott.irdeto.com/Widevine/getlicense?CrmId=tatasky&AccountId=tatasky&ContentId=400000077&ls_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNvbnRyb2xfc2lnbmluZ19rZXlfcHJvZHVjdGlvbl8xNzIyOTY3MDk3ODc1In0.eyJzdWIiOiIxMTcyNTEyNzcyIiwiaXNlIjp0cnVlLCJqdGkiOiIwMGJlMTMxMS03MjI1LTRjNTgtOGZmZS02OGJkNDkwNDY1MTMiLCJhaWQiOiJ0YXRhc2t5IiwiZXhwIjoxNzQ0MzA4MjIxLCJuYW1lIjoidXNoYSAuIiwiaWF0IjoxNzQ0MjkzNTIxLCJlbnQiOlt7ImVwaWQiOiJTdWJzY3JpcHRpb25fQnJvd3Nlcl9TdHJlYW1pbmciLCJiaWQiOiIxMDAwMDAwOTI3In1dLCJpc3MiOiJ0cG1hX3dlYiJ9.vjGaC1J-mA_7FonSUdxppyETyZpeNmjctZ6aqVolZuI",
-            data=challenge
+            data=challenge,
+            proxies=proxies,
+            timeout=10
         )
         licence.raise_for_status()
 
