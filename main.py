0f32eddbc323568b0f882c11834c307ecfce5dc9
sushnimo <33617992+sushnimo@users.noreply.github.com>
2025-04-14 18:13:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_keys() -> Response:
         challenge = cdm.get_license_challenge(session_id, pssh)
 
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
         }
 
         # Send license challenge
